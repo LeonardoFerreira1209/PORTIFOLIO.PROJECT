1a96ad2e6c56956eac5a6d8051952ff084e22877
Leonardo Ferreira de Almeida <Leo.Ferreira30@outlook.com>
2023-10-30 08:43:18 -0300

    Update ExtensionsConfigurations.cs

--- PORTIFOLIO/APPLICATION/APPLICATION/CONFIGURATIONS/ExtensionsConfigurations.cs
+++ PORTIFOLIO/APPLICATION/APPLICATION/CONFIGURATIONS/ExtensionsConfigurations.cs
@@ -224,7 +224,7 @@ public static class ExtensionsConfigurations
                 ValidateAudience = true,
                 ValidateLifetime = true,
                 ValidateIssuerSigningKey = true,
-                ClockSkew = TimeSpan.FromHours(4),
+                ClockSkew = TimeSpan.FromHours(3),
 
                 ValidIssuer = configurations.GetValue<string>("Auth:ValidIssuer"),
                 ValidAudience = configurations.GetValue<string>("Auth:ValidAudience"),
